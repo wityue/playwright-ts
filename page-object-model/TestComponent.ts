81ff3ad977b072a874161a94ca18d0c1bb8557be
zhiyong.yue <zhiyong.yue@onecontract-cloud.com>
2023-04-28 17:50:05 +0800

    优化

--- page-object-model/TestComponent.ts
+++ page-object-model/TestComponent.ts
@@ -7,8 +7,34 @@ export default abstract class TestComponent {
 		this.page = page;
 	}
 
-	async fill_快捷操作_填写表单() {
-
+	async fill_快捷操作_填写表单(fields: Record<string, string | number>) {
+		for (const [field, value] of Object.entries(fields)) {
+			const input = await this.page.$(`input[name="${field}"]`);
+			if (!input) {
+				throw new Error(`Input field "${field}" not found`);
+			} 
+			const inputType = await input.evaluate((el) => {
+				const classList = el.classList;
+				if (classList.contains('ocant')) {
+					return 'OCANT';
+				} else {
+					const parentClassList = el.parentElement?.classList;
+					if (parentClassList?.contains('ocant')) {
+						return 'OCANT';
+					} else {
+						return 'C7N';
+					}
+				}
+			});
+			const inputType = await input.evaluate((el) => el.getAttribute('type'));
+			if (inputType === 'text') {
+				await input.fill(value);
+			} else if (inputType === 'select-one') {
+				await input.selectOption({ value });
+			} else {
+				throw new Error(`Unsupported input type "${inputType}" for field "${field}"`);
+			}
+		}
 	}
 
 	async fill_快捷操作_填写表格() {
