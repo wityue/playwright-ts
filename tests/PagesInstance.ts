708ee24bc4b01b47958da6b64ceb0dc62c0b27c1
zhiyong.yue <zhiyong.yue@onecontract-cloud.com>
2023-05-13 20:01:36 +0800

    rename pages name

--- tests/PagesInstance.ts
+++ tests/PagesInstance.ts
@@ -1,15 +1,26 @@
 import type { Page } from 'playwright-core';
-import 登录页 from '../page-object-model/登录页';
-import 合同查询 from '../page-object-model/合同查询';
+import LoginPage from '../page-object-model/LoginPage';
+import DashboardPage from '../page-object-model/DashboardPage';
+import PopupDetailPage from '../page-object-model/PopUpDetailPage';
 
 export default class PagesInstance {
     page: Page;
-    登录页: 登录页;
-    合同查询: 合同查询;
+    LoginPage: LoginPage;
+    DashboardPage: DashboardPage;
+    PopupDetailPage: PopupDetailPage
     
     constructor(page: Page) {
       this.page = page;
-      this.登录页 = new 登录页(this.page);
-      this.合同查询 = new 合同查询(this.page);
+      this.LoginPage = new LoginPage(this.page);
+      this.DashboardPage = new DashboardPage(this.page);
+      this.PopupDetailPage = new PopupDetailPage(this.page)
+    }
+
+    newPagesInstance(page:Page) {
+      return new PagesInstance(page)
+    }
+
+    newPopupDetailPage(page:Page){
+      return this.newPopupDetailPage(page)
     }
 }
