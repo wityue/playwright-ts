cf1a120e9e972c9a8ad4787e4dc096bd1de8a218
zhiyong.yue <zhiyong.yue@onecontract-cloud.com>
2023-08-30 11:16:27 +0800

    增加maskTag变量,用于临时禁止增加蒙层

--- tools/apiObserver.js
+++ tools/apiObserver.js
@@ -1,5 +1,8 @@
 (function () {
   "use strict";
+  const win = window.unsafeWindow || document.defaultView || window;
+  // 用于控制是否增加蒙层, 临时禁止:await page.evaluate("window.maskTag=0") 恢复:await page.evaluate("window.maskTag=1")
+  win.maskTag = 1;
   let mask = document.createElement("div");
   mask.style =
     "position: fixed;top: 0;right: 0;bottom: 0;left: 0;z-index: 1000;height: 100%;background-color: rgba(0,0,0,.0)";
@@ -19,7 +22,7 @@
     //   lastEntry.name.includes("https://cdn")
     // ) {
     if (lastEntry.name.includes("https://cdn")) {
-      window.lastResponseEndTime = Date.now();
+      win.lastResponseEndTime = Date.now();
     }
   });
   apiObserver.observe({ entryTypes: ["resource"] });
@@ -32,36 +35,38 @@
   ]);
 
   ajaxHooker.hook((request) => {
-    window.apiCounter++;
+    win.apiCounter++;
     try {
-      apiCounterElement.textContent = "apiCount:" + window.apiCounter;
-      if (!document.getElementById("networkIdleMask")) {
-        mask.appendChild(apiCounterElement);
-        document.body.appendChild(mask);
-      }
-      request.response = (res) => {
-        if (
-          window.apiCounter === 0 &&
-          document.getElementById("networkIdleMask")
-        ) {
-          const timeOut = 10000;
-          const startTime = Date.now();
-          const intervalId = setInterval(() => {
-            if (
-              !window.apiCounter &&
-              Date.now() - window.lastResponseEndTime > 80
-            ) {
-              if (document.getElementById("networkIdleMask")) {
-                document.getElementById("networkIdleMask").remove();
-              }
-              clearInterval(intervalId);
-            }
-            if (Date.now() - startTime > timeOut) {
-              clearInterval(intervalId);
-            }
-          }, 80);
+      if (win.maskTag) {
+        apiCounterElement.textContent = "apiCount:" + win.apiCounter;
+        if (!document.getElementById("networkIdleMask")) {
+          mask.appendChild(apiCounterElement);
+          document.body.appendChild(mask);
         }
-      };
+        request.response = (res) => {
+          if (
+            win.apiCounter === 0 &&
+            document.getElementById("networkIdleMask")
+          ) {
+            const timeOut = 10000;
+            const startTime = Date.now();
+            const intervalId = setInterval(() => {
+              if (
+                !win.apiCounter &&
+                Date.now() - win.lastResponseEndTime > 80
+              ) {
+                if (document.getElementById("networkIdleMask")) {
+                  document.getElementById("networkIdleMask").remove();
+                }
+                clearInterval(intervalId);
+              }
+              if (Date.now() - startTime > timeOut) {
+                clearInterval(intervalId);
+              }
+            }, 80);
+          }
+        };
+      }
     } catch (error) {
       console.log(error);
     }
