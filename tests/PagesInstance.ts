f7abfc796b9d5049e61ae43d78c866bf5230faa3
zhiyong.yue <zhiyong.yue@onecontract-cloud.com>
2023-09-01 15:36:11 +0800

    修改逻辑为第一次调用对应Page时进行实例化,同时增加切换页面及临时停用注入脚本添加mask方法

--- tests/PagesInstance.ts
+++ tests/PagesInstance.ts
@@ -1,29 +1,75 @@
 import type { Page } from "playwright-core";
+import PageComponent from "../page-object-model/PageComponent";
 import LoginPage from "../page-object-model/LoginPage";
 import DashboardPage from "../page-object-model/DashboardPage";
 import PopupDetailPage from "../page-object-model/PopUpDetailPage";
 import MyProject from "../page-object-model/MyProject";
 
+// 所有页面实例,当第一次使用实进行实例化,当Page修改后会重新进行实例化,确定页面实例为最新Page对象
 export default class PagesInstance {
   page: Page;
+  private defalutPage: Page;
+  private previousPage: Page;
   LoginPage: LoginPage;
   DashboardPage: DashboardPage;
   PopupDetailPage: PopupDetailPage;
   MyProject: MyProject;
-
+  protected pagesClasses: { [key: string]: any } = {
+    LoginPage,
+    DashboardPage,
+    PopupDetailPage,
+    MyProject,
+  };
   constructor(page: Page) {
     this.page = page;
-    this.LoginPage = new LoginPage(this.page);
-    this.DashboardPage = new DashboardPage(this.page);
-    this.PopupDetailPage = new PopupDetailPage(this.page);
-    this.MyProject = new MyProject(this.page);
+    this.defalutPage = page;
+    return new Proxy(this, {
+      get(target, prop) {
+        if (
+          typeof target[prop] === "undefined" ||
+          (target[prop] instanceof PageComponent &&
+            target[prop].page !== target.page)
+        ) {
+          delete target[prop];
+          target[prop] = new target.pagesClasses[prop as string](target.page);
+        }
+        return target[prop];
+      },
+    });
   }
 
-  newPagesInstance(page: Page) {
-    return new PagesInstance(page);
+  // 切换页面,当不传参时,则切换至实例化类时的默认Page
+  //
+  // 参数:
+  //   - page: 要切换到的页面
+  //   - previousPage: 是否切换到上一个页面
+  
+  switchToPage(page?: Page, previousPage?: false) {
+    if (page) {
+      this.previousPage = page;
+      this.page = page;
+      return;
+    } else if (previousPage) {
+      this.page = this.previousPage;
+      return;
+    } else {
+      this.page = this.defalutPage;
+    }
   }
 
-  newPopupDetailPage(page: Page) {
-    return this.newPopupDetailPage(page);
+  // 临时停用mask,避免添加mask对playwright操作造成意外影响
+  //
+  // 参数:
+  //   - callback: 要执行的回调函数
+  //
+  // 返回值:
+  //   - Promise<R>: 异步回调函数的返回值
+  async temporarilyDisableMask<R>(callback: () => Promise<R>): Promise<R> {
+    await this.page.evaluate("window.maskTag=0");
+    try {
+      return await callback();
+    } finally {
+      await this.page.evaluate("window.maskTag=1");
+    }
   }
 }
